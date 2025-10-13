a7c7e779d0c9b34555ad70adf61546a9be66244e
gomdol <oioifor@naver.com>
2025-10-13 14:14:25 +0900

    feat: add transactionsService 조회

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DataSource } from 'typeorm';
+import { Transaction, TransactionStatus } from './entities/transaction.entity';
+import { TransactionItem } from './entities/transaction-item.entity';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+
+
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+    constructor(
+        @InjectRepository(Transaction)
+        private transactionsRepository: Repository<Transaction>,
+        @InjectRepository(TransactionItem)
+        private transactionItemsRepository: Repository<TransactionItem>,
+        private dataSource: DataSource,
+    ) {}
+
+    async findAll(): Promise<Transaction[]> {
+        return await this.transactionsRepository.find({
+            relations: ['client', 'items', 'items.product'],
+            order: { transactionDate: 'DESC' },
+        });
+    }
+
+    async findOne(id: number): Promise<Transaction> {
+        const transaction = await this.transactionsRepository.findOne({
+            where: { id },
+            relations: ['client', 'items', 'items.product'],
+        });
+
+        if (!transaction) {
+            throw new NotFoundException(`거래 전표 ID ${id}를 찾을 수 없습니다.`);
+        }
+
+        return transaction;
+    }
+}
