4a495b74a1a9c19859153f40ba2034725b960348
gomdol <oioifor@naver.com>
2025-10-13 11:00:06 +0900

    feat: add clients controller

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { ClientsService } from './clients.service';
+import { CreateClientDto } from './dto/create-client.dto';
+import { UpdateClientDto } from './dto/update-client.dto';
 
 @Controller('clients')
-export class ClientsController {}
+export class ClientsController {
+    constructor(private readonly clientsService: ClientsService) {}
+
+    @Get()
+    findAll() {
+        return this.clientsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.clientsService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() createClientDto: CreateClientDto) {
+        return this.clientsService.create(createClientDto);
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateClientDto: UpdateClientDto) {
+        return this.clientsService.update(id, updateClientDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number) {
+        return this.clientsService.remove(id);
+    }
+
+    @Get(':id/statistics')
+    getStatistics(@Param('id', ParseIntPipe) id: number) {
+        return this.clientsService.getClientStatistics(id);
+    }
+}
