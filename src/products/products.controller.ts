2a54292988126535205c65539f2090ac5a02c679
gomdol <oioifor@naver.com>
2025-10-13 14:11:59 +0900

    feat: add product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {}
+
+    @Get()
+    findAll() {
+        return this.productsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.productsService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() createProductDto: CreateProductDto) {
+        return this.productsService.create(createProductDto);
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
+        return this.productsService.update(id, updateProductDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number) {
+        return this.productsService.remove(id);
+    }
+
+    @Get(':id/statistics')
+    getStatistics(@Param('id', ParseIntPipe) id: number) {
+        return this.productsService.getProductStatistics(id);
+    }
+}
